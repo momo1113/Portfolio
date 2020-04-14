1eeaeb7945f333e6ef63197a064c09b68141395c
Momo Sun <mosun1113@gmail.com>
2020-04-13 18:04:57 -0700

    contact page

--- src/components/ContactPage.js
+++ src/components/ContactPage.js
@@ -1,8 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import profile from "../img/profile_pic.png";
+import github from "../img/github.png";
+import linkedin from "../img/linkedin.png";
+import email from "../img/mail.png";
 
 const ContactPage = () => (
-  <div>
+  <div className="contact">
     {/* header */}
     <div className="header">
       <div className="header--item">
@@ -12,25 +16,46 @@ const ContactPage = () => (
       </div>
 
       <div className="header--item">
-        <Link to="" className="header--item__text">
+        <Link to="/academy" className="header--item__text">
           Education
         </Link>
       </div>
 
       <div className="header--item">
-        <Link to="" className="header--item__text">
+        <Link to="/portfolio" className="header--item__text">
           Portfolio
         </Link>
       </div>
     </div>
 
     {/* pictures */}
+    <div className="info--container">
+      <img src={profile} alt="self-profile" className="info--container__pic" />
+    </div>
 
-    <div>
-      <div>
-        <img></img>
-      </div>
-      <div></div>
+    {/* social media */}
+    <div className="social--container">
+      <a href="https://www.linkedin.com/in/momo-sun-626766179/">
+        <img
+          src={linkedin}
+          alt="linkedin"
+          className="social--container__item social--container__linkedin"
+        />
+      </a>
+      <a href="https://github.com/momo1113">
+        <img
+          src={github}
+          alt="github"
+          className=" social--container__item social--container__github"
+        />
+      </a>
+      <a href={`mailto: mosun1113@gmail.com`}>
+        <img
+          src={email}
+          alt="gemail"
+          className=" social--container__item social--container__email"
+        />
+      </a>
     </div>
   </div>
 );
